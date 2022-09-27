15a4745284b2360375e154f66a455f740e28df70
Sean Richardson <59182147+seanrichardson95@users.noreply.github.com>
2022-09-27 17:49:21 -0400

    exercise 3 post solution

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -45,6 +45,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
+  align-items: baseline;
 `;
 
 const LeftColumn = styled.div`
